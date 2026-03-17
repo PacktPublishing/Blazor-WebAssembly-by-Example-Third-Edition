857b574761074fc6f1fd6a83e682ab5d2d6962e3
Toi Wright <toi@onestopdesigns.com>
2026-03-16 21:52:20 -0500

    Chapter 3

---
+++ Chapter 03/Ch3_App/Ch3_App/Program.cs
@@ -0,0 +1,13 @@
+using Ch3_App;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Logging.SetMinimumLevel(LogLevel.Trace);
+
+await builder.Build().RunAsync();
