57a9d4e8a086ae0bd918fe3b06a7c7542466db2c
Toi Wright <toi@onestopdesigns.com>
2026-05-12 22:36:12 -0500

    Chapter 13

---
+++ Chapter 13/ClaimsDashboard/ClaimsDashboard.Client/Services/AuthorizeMessageHandler.cs
@@ -0,0 +1,30 @@
+﻿using ClaimsDashboard.Client.Interfaces;
+using System.Net.Http.Headers;
+
+namespace ClaimsDashboard.Client.Services
+{
+    public class AuthorizeMessageHandler : DelegatingHandler
+    {
+        private readonly ITokenStorageService tokenStorage;
+
+        public AuthorizeMessageHandler(ITokenStorageService tokenStorage)
+        {
+            this.tokenStorage = tokenStorage;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            var token = await tokenStorage.GetTokenAsync();
+
+            if (!string.IsNullOrWhiteSpace(token))
+            {
+                request.Headers.Authorization =
+                    new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ Chapter 13/ClaimsDashboard/ClaimsDashboard.Shared/LoginResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace ClaimsDashboard.Shared
+{
+    public record LoginResponse(string Token);
+}
