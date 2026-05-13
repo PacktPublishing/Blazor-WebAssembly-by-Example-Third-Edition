16ce9ab678047a5778b4b725cd27fce004675d67
Toi Wright <toi@onestopdesigns.com>
2026-05-12 22:33:30 -0500

    Chapter 11

---
+++ Chapter 11/Ch11_App/Ch11_App/Models/Expense.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ch11_App.Models
+{
+    public class Expense
+    {
+        public string Id { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime? Date { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string? Vendor { get; set; }
+
+        [Required]
+        [Range(0, 500, ErrorMessage = "The Amount must be <= 500 to be approved")]
+        public decimal? Amount { get; set; }
+
+    }
+}
